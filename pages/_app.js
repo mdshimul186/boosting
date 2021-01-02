1d67a1ec39125992175007c1bd6463a638e5e9e5
unknown <186mdshimul@gmail.com>
2021-01-02 16:30:02 +0600

    sd

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 
-import '../static/css/responsive.css'
-import '../static/css/responsive.css'
 import '../static/css/bootstrap.min.css'
+
 import '../static/css/slick.css'
 import '../static/css/animate.css'
 import '../static/css/flaticon.css'
@@ -11,7 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-accessible-accordion/dist/fancy-example.css';
 import '../node_modules/react-modal-video/css/modal-video.min.css'
 import 'react-image-lightbox/style.css';
-import '../static/styles/style.css'
+
+
 // If you want to change the theme color you should comment out above line and uncomment the below line and change the color names from list
 /*
 * brink-pink-style.css
@@ -19,8 +19,11 @@ import '../static/styles/style.css'
 * purple-style.css
 */
 // import '../static/styles/brink-pink-style.css'
-import '../static/css/responsive.css'
+
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import '../static/styles/style.css'
+import '../static/css/responsive.css'
+
 
 import { Provider } from 'react-redux';
 import App from 'next/app'
