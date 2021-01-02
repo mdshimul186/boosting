e9c35c572440c84b980c38b8eaf710b7e06bdf65
unknown <186mdshimul@gmail.com>
2021-01-02 16:08:22 +0600

    zdgds

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const next = require('next');
+const path = require('path');
+const bodyParser = require('body-parser');
+const keys = require("./server/config/keys");
+const stripe = require('stripe')(keys.stripeSecretKey);
+const mailer = require('./mailer');
+
+const dev = process.env.NODE_ENV !== 'production';
+
+const app = next({ dir: '.', dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = express();
+        // Static files
+        // https://github.com/zeit/next.js/tree/4.2.3#user-content-static-file-serving-eg-images
+    server.use('/images', express.static(path.join(__dirname, 'images'), {
+        maxAge: dev ? '0' : '365d'
+    }));
+
+    server.use(bodyParser.json());
+
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    });
+
+    server.post('/api/contact', (req, res) => {
+        const { name, email, phone, subject, text } = req.body;
+        mailer({name, email, phone, subject, text}).then(() => {
+            res.send('success')
+        }).catch(error => {
+            res.status(422).send(error)
+        });
+    });
+
+    server.post('/api/stripe/checkout', async (req, res) => {
+        await stripe.charges.create({
+            amount: req.body.amount,
+            currency: 'usd',
+            description: 'StartP Next Landing Page Templates',
+            source: req.body.token.id
+        });
+        res.send({})
+    });
+
+    const PORT = process.env.PORT || 3000;
+
+    server.listen(PORT, (err) => {
+        if (err) throw err
+        console.log(`> Read on http://localhost:${PORT}`)
+    });
+})
